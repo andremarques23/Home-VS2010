2bd05e86173cef5c552af9b7755acb3e52befe67
André Marques de Araújo <andremarques023@gmail.com>
2012-06-21 03:33:31 -0300

    Home.VS2010.Common.Services project - Creation
    Home.VS2010.Common.Services project added to repo.

--- Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Properties/AssemblyInfo.cs
+++ Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Home.VS2010.Common.Debugging")]
+[assembly: AssemblyTitle("Home.VS2010.Common.Services")]
 [assembly: AssemblyDescription("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("BD962C16-764F-468E-B7C2-B1778C10935D")]
+[assembly: Guid("C8E5F367-3D97-4626-8271-9A2E7FCD9DBF")]
