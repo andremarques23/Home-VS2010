e83c1a1423b4d0558744b12265816fb0161c2de6
André Marques de Araújo <andremarques023@gmail.com>
2012-06-21 04:18:24 -0300

    Common.Services - Modification
    Typed ServiceHost class added to the project.

---
+++ Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Hosting/ServiceHost`1.cs
@@ -0,0 +1,37 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ServiceHost{T}.cs" company="Home">
+//     Home development project. No rights reserved.
+// </copyright>
+// <author>André Marques de Araújo</author>
+//-----------------------------------------------------------------------
+
+namespace Home.VS2010.Common.Services.Hosting
+{
+    using System;
+    using System.ServiceModel;
+
+    /// <summary>
+    /// Provides a typed host for services.
+    /// </summary>
+    /// <typeparam name="T">The type of hosted service.</typeparam>
+    public class ServiceHost<T> : ServiceHost
+    {
+        /// <summary>
+        /// Initializes a new instance of the ServiceHost class.
+        /// </summary>
+        public ServiceHost()
+            : base(typeof(T))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the System.ServiceModel.ServiceHost class with 
+        /// the type of service and its base addresses specified.
+        /// </summary>
+        /// <param name="baseAddresses">An array of type System.Uri that contains the base addresses for the hosted service.</param>
+        public ServiceHost(params Uri[] baseAddresses)
+            : base(typeof(T), baseAddresses)
+        {
+        }
+    }
+}
