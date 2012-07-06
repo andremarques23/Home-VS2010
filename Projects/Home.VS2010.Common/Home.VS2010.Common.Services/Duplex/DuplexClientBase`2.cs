9e308e3020dd5a11174377b8e6315bd1b945b1cc
André Marques de Araújo <andremarques023@gmail.com>
2012-07-06 00:21:14 -0300

    Common.Services - Modification
    DuplexClientBase<T> and InstanceContext classes were extended.

---
+++ Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Duplex/DuplexClientBase`2.cs
@@ -0,0 +1,146 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DuplexClientBase.cs" company="Home">
+//     Home development project. No rights reserved.
+// </copyright>
+// <author>André Marques de Araújo</author>
+//-----------------------------------------------------------------------
+
+namespace Home.VS2010.Common.Services.Duplex
+{
+    using System.ServiceModel;
+    using System.ServiceModel.Channels;
+    using System.ServiceModel.Description;
+
+    /// <summary>
+    /// Used to create a channel to a duplex service and associate that channel with a callback object.
+    /// </summary>
+    /// <typeparam name="T">The type of the channel to be created.</typeparam>
+    /// <typeparam name="C">The callback object that implements the service instance.</typeparam>
+    public abstract class DuplexClientBase<T, C> : DuplexClientBase<T>
+        where T : class
+    {
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="instanceContext">An System.ServiceModel.InstanceContext object that associates the callback object with the channel to the service.</param>
+        protected DuplexClientBase(InstanceContext<C> instanceContext)
+            : base(instanceContext)
+        { 
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="instanceContext">An System.ServiceModel.InstanceContext object that associates the callback object with the channel to the service.</param>
+        /// <param name="serviceEndpoint">The service endpoint.</param>
+        protected DuplexClientBase(InstanceContext<C> instanceContext, ServiceEndpoint serviceEndpoint)
+            : base(instanceContext, serviceEndpoint)
+        { 
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="instanceContext">An System.ServiceModel.InstanceContext object that associates the callback object with the channel to the service.</param>
+        /// <param name="endpointConfigurationName">The name of the client endpoint information in the application configuration file.</param>
+        protected DuplexClientBase(InstanceContext<C> instanceContext, string endpointConfigurationName)
+            : base(instanceContext, endpointConfigurationName)
+        { 
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="instanceContext">An System.ServiceModel.InstanceContext object that associates the callback object with the channel to the service.</param>
+        /// <param name="endpointConfigurationName">The name of the client endpoint information in the application configuration file.</param>
+        /// <param name="endpointRemoteAddress">The address of the service endpoint to use.</param>
+        protected DuplexClientBase(InstanceContext<C> instanceContext, string endpointConfigurationName, string endpointRemoteAddress)
+            : base(instanceContext, endpointConfigurationName, endpointRemoteAddress)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="instanceContext">An System.ServiceModel.InstanceContext object that associates the callback object with the channel to the service.</param>
+        /// <param name="endpointConfigurationName">The name of the client endpoint information in the application configuration file.</param>
+        /// <param name="endpointRemoteAddress">The service endpoint address to use.</param>
+        protected DuplexClientBase(InstanceContext<C> instanceContext, string endpointConfigurationName, EndpointAddress endpointRemoteAddress)
+            : base(instanceContext, endpointConfigurationName, endpointRemoteAddress)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="instanceContext">An System.ServiceModel.InstanceContext object that associates the callback object with the channel to the service.</param>
+        /// <param name="binding">The binding with which to call the service.</param>
+        /// <param name="endpointRemoteAddress">The service endpoint address to use.</param>
+        protected DuplexClientBase(InstanceContext<C> instanceContext, Binding binding, EndpointAddress endpointRemoteAddress)
+            : base(instanceContext, binding, endpointRemoteAddress)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="callbackInstance">An object used to create the instance context that associates the callback object with the channel to the service.</param>
+        protected DuplexClientBase(object callbackInstance)
+            : base(callbackInstance)
+        { 
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="callbackInstance">An object used to create the instance context that associates the callback object with the channel to the service.</param>
+        /// <param name="serviceEndpoint">The service endpoint.</param>
+        protected DuplexClientBase(object callbackInstance, ServiceEndpoint serviceEndpoint)
+            : base(callbackInstance, serviceEndpoint)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="callbackInstance">An object used to create the instance context that associates the callback object with the channel to the service.</param>
+        /// <param name="endpointConfigurationName">The name of the client endpoint information in the application configuration file.</param>
+        protected DuplexClientBase(object callbackInstance, string endpointConfigurationName)
+            : base(callbackInstance, endpointConfigurationName)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="callbackInstance">An object used to create the instance context that associates the callback object with the channel to the service.</param>
+        /// <param name="endpointConfigurationName">The name of the client endpoint information in the application configuration file.</param>
+        /// <param name="endpointRemoteAddress">The address of the service endpoint to use.</param>
+        protected DuplexClientBase(object callbackInstance, string endpointConfigurationName, string endpointRemoteAddress)
+            : base(callbackInstance, endpointConfigurationName, endpointRemoteAddress)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="callbackInstance">An object used to create the instance context that associates the callback object with the channel to the service.</param>
+        /// <param name="endpointConfigurationName">The name of the client endpoint information in the application configuration file.</param>
+        /// <param name="endpointRemoteAddress">The address of the service endpoint to use.</param>
+        protected DuplexClientBase(object callbackInstance, string endpointConfigurationName, EndpointAddress endpointRemoteAddress)
+            : base(callbackInstance, endpointConfigurationName, endpointRemoteAddress)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DuplexClientBase class.
+        /// </summary>
+        /// <param name="callbackInstance">An object used to create the instance context that associates the callback object with the channel to the service.</param>
+        /// <param name="binding">The binding with which to call the service.</param>
+        /// <param name="endpointRemoteAddress">The service endpoint address to use.</param>
+        protected DuplexClientBase(object callbackInstance, Binding binding, EndpointAddress endpointRemoteAddress)
+            : base(callbackInstance, binding, endpointRemoteAddress)
+        {
+        }
+    }
+}
---
+++ Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Duplex/InstanceContext`1.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="InstanceContext.cs" company="Home">
+//     Home development project. No rights reserved.
+// </copyright>
+// <author>André Marques de Araújo</author>
+//-----------------------------------------------------------------------
+
+namespace Home.VS2010.Common.Services.Duplex
+{
+    using System.ServiceModel;
+
+    /// <summary>
+    /// Represents the context information for a service instance.
+    /// </summary>
+    /// <typeparam name="T">The callback object that implements the service instance.</typeparam>
+    public class InstanceContext<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the InstanceContext class.
+        /// </summary>
+        /// <param name="callbackInstance">The callback that implements the service instance.</param>
+        public InstanceContext(T callbackInstance)
+        {
+            this.Context = new InstanceContext(callbackInstance);
+        }
+
+        /// <summary>
+        /// Gets the context information for a service instance.
+        /// </summary>
+        public InstanceContext Context
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the instance of the service for the instance context.
+        /// </summary>
+        public T ServiceInstance
+        {
+            get
+            {
+                return (T)this.Context.GetServiceInstance();
+            }
+        }
+
+        /// <summary>
+        /// Releases the service instance.
+        /// </summary>
+        public void ReleaseServiceInstance()
+        {
+            this.Context.ReleaseServiceInstance();
+        }
+    }
+}
