7b1b70e5e1411cdee5bf0cabb1163249f869416c
André Marques de Araújo <andremarques023@gmail.com>
2012-07-08 19:09:08 -0300

    Common.Services - InProcServiceFactory.cs
    MethodImpl(MethodImplOptions.Synchronized) attribute added to some
    operations.

--- Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Hosting/InProcServiceFactory.cs
+++ Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Hosting/InProcServiceFactory.cs
@@ -9,9 +9,11 @@ namespace Home.VS2010.Common.Services.Hosting
 {
     using System;
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
     using System.ServiceModel;
     using System.ServiceModel.Channels;
     using System.ServiceModel.Description;
+    using Duplex;
     using Extensions;
     using Resources;
 
@@ -62,6 +64,7 @@ namespace Home.VS2010.Common.Services.Hosting
         /// <typeparam name="I">The type of the service contract.</typeparam>
         /// <typeparam name="S">The type of the implemented service contract.</typeparam>
         /// <returns>The channel created by the factory.</returns>
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public static I CreateChannel<I, S>()
             where I : class
             where S : class, I
@@ -69,6 +72,42 @@ namespace Home.VS2010.Common.Services.Hosting
             HostEndpointPair hostAddressPair = GetHostEndpointPair<I, S>();
 
             return ChannelFactory<I>.CreateChannel(Binding, hostAddressPair.EndpointAddress);
+        }
+
+        /// <summary>
+        /// Creates a channel of a specified service contract type.
+        /// </summary>
+        /// <typeparam name="I">The type of the service contract.</typeparam>
+        /// <typeparam name="S">The type of the implemented service contract.</typeparam>
+        /// <typeparam name="C">The callback that implements the service instance.</typeparam>
+        /// <param name="callbackObject">The callback object that implements the service instance.</param>
+        /// <returns>The channel created by the factory.</returns>
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static I CreateChannel<I, S, C>(C callbackObject)
+            where I : class
+            where S : class, I
+        {
+            InstanceContext<C> instanceContext = new InstanceContext<C>(callbackObject);
+
+            return CreateChannel<I, S, C>(instanceContext);
+        }
+
+        /// <summary>
+        /// Creates a channel of a specified service contract type.
+        /// </summary>
+        /// <typeparam name="I">The type of the service contract.</typeparam>
+        /// <typeparam name="S">The type of the implemented service contract.</typeparam>
+        /// <typeparam name="C">The callback that implements the service instance.</typeparam>
+        /// <param name="instanceContext">The context information for the service instance.</param>
+        /// <returns>The channel created by the factory.</returns>
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static I CreateChannel<I, S, C>(InstanceContext<C> instanceContext)
+            where I : class
+            where S : class, I
+        {
+            HostEndpointPair hostAddressPair = GetHostEndpointPair<I, S>();
+
+            return DuplexChannelFactory<I, C>.CreateChannel(instanceContext, Binding, hostAddressPair.EndpointAddress);
         }
 
         /// <summary>
@@ -96,6 +135,7 @@ namespace Home.VS2010.Common.Services.Hosting
 
         /// <summary>
         /// Sets run-time throughput settings to the largest possible values of an System.Int32.
+        /// This method should only be called before a service instance is created.
         /// </summary>
         /// <typeparam name="S">The service type.</typeparam>
         public static void MaxOutThrottle<S>()
@@ -104,10 +144,12 @@ namespace Home.VS2010.Common.Services.Hosting
         }
 
         /// <summary>
-        /// Sets run-time throughput settings for service performance tunning.
+        /// Sets run-time throughput settings for service performance tunning. 
+        /// This method should only be called before a service instance is created.
         /// </summary>
         /// <typeparam name="S">The service type.</typeparam>
         /// <param name="serviceThrottlingBehavior">System.ServiceModel.Description.ServiceThrottlingBehavior containing the settings.</param>
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public static void SetThrottle<S>(ServiceThrottlingBehavior serviceThrottlingBehavior)
         {
             serviceThrottlingBehaviors[typeof(S)] = serviceThrottlingBehavior;
@@ -115,6 +157,7 @@ namespace Home.VS2010.Common.Services.Hosting
 
         /// <summary>
         /// Sets run-time throughput settings for service performance tunning.
+        /// This method should only be called before a service instance is created.
         /// </summary>
         /// <typeparam name="S">The service type.</typeparam>
         /// <param name="maxConcurrentCalls">The maximum number of messages actively processing across a System.ServiceModel.ServiceHost.</param>
