46027359e9a74bb952979ce5609774fd756d7d8f
André Marques de Araújo <andremarques023@gmail.com>
2012-07-10 02:57:45 -0300

    Common.Services - BindingRequirementsAttribute.cs and BindingValidations.cs
    Support for binding validation checks via attribute added.

---
+++ Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Attributes/BindingRequirementsAttribute.cs
@@ -0,0 +1,135 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BindingRequirementsAttribute.cs" company="Home">
+//     Home development project. No rights reserved.
+// </copyright>
+// <author>André Marques de Araújo</author>
+//-----------------------------------------------------------------------
+
+namespace Home.VS2010.Common.Services.Attributes
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.ServiceModel;
+    using System.ServiceModel.Channels;
+    using System.ServiceModel.Description;
+    using System.ServiceModel.Dispatcher;
+    using Validation;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class BindingRequirementsAttribute : Attribute, IServiceBehavior, IEndpointBehavior
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether reliable session is required.
+        /// </summary>
+        public bool RequireReliability
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether transaction flow is required.
+        /// </summary>
+        public bool RequireTransactionFlow
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a binding which supports only communication between WCF applications is required.
+        /// </summary>
+        public bool RequireOnlyWcfCommunication
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Provides the ability to pass custom data to binding elements to support the contract implementation.
+        /// </summary>
+        /// <param name="serviceDescription">The service description of the service.</param>
+        /// <param name="serviceHostBase">The host of the service.</param>
+        /// <param name="endpoints">The service endpoints.</param>
+        /// <param name="bindingParameters">Custom objects to which binding elements have access.</param>
+        void IServiceBehavior.AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        /// <summary>
+        /// Implement to pass data at runtime to bindings to support custom behavior.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to modify.</param>
+        /// <param name="bindingParameters">The objects that binding elements require to support the behavior.</param>
+        void IEndpointBehavior.AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        /// <summary>
+        /// Provides the ability to change run-time property values or insert custom extension objects such as error handlers, message or parameter interceptors, security extensions, and other custom extension objects.
+        /// </summary>
+        /// <param name="serviceDescription">The service description.</param>
+        /// <param name="serviceHostBase">The host that is currently being built.</param>
+        void IServiceBehavior.ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+        }
+
+        /// <summary>
+        /// Implements a modification or extension of the client across an endpoint.
+        /// </summary>
+        /// <param name="endpoint">The endpoint that is to be customized.</param>
+        /// <param name="clientRuntime">The client runtime to be customized.</param>
+        void IEndpointBehavior.ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+        }
+
+        /// <summary>
+        /// Implements a modification or extension of the service across an endpoint.
+        /// </summary>
+        /// <param name="endpoint">The endpoint that exposes the contract.</param>
+        /// <param name="endpointDispatcher">The endpoint dispatcher to be modified or extended.</param>
+        void IEndpointBehavior.ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+        }
+
+        /// <summary>
+        /// Provides the ability to inspect the service host and the service description to confirm that the service can run successfully.
+        /// </summary>
+        /// <param name="serviceDescription">The service description.</param>
+        /// <param name="serviceHostBase">The service host that is currently being constructed.</param>
+        void IServiceBehavior.Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            IEndpointBehavior endpointBehavior = this;
+            
+            foreach (var endpoint in serviceDescription.Endpoints)
+            {
+                endpointBehavior.Validate(endpoint);
+            }
+        }
+
+        /// <summary>
+        /// Implement to confirm that the endpoint meets some intended criteria.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to validate.</param>
+        void IEndpointBehavior.Validate(ServiceEndpoint endpoint)
+        {
+            if (this.RequireReliability)
+            {
+                BindingValidations.ValidateReliabilityEnabled(endpoint);
+            }
+
+            if (this.RequireTransactionFlow)
+            {
+                BindingValidations.ValidateTransactionFlowEnabled(endpoint);
+            }
+
+            if (this.RequireOnlyWcfCommunication)
+            {
+                BindingValidations.ValidateOnlyWcfCommunication(endpoint);
+            }
+        }
+    }
+}
---
+++ Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Validation/BindingValidations.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BindingValidations.cs" company="Home">
+//     Home development project. No rights reserved.
+// </copyright>
+// <author>André Marques de Araújo</author>
+//-----------------------------------------------------------------------
+
+namespace Home.VS2010.Common.Services.Validation
+{
+    using System;
+    using System.ServiceModel;
+    using System.ServiceModel.Channels;
+    using System.ServiceModel.Description;
+    using Resources;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public static class BindingValidations
+    {
+        public static void ValidateReliabilityEnabled(ServiceEndpoint serviceEndpoint)
+        {
+            Binding binding = serviceEndpoint.Binding;
+
+            if (binding is NetNamedPipeBinding)
+            {
+                return;
+            }
+
+            if (binding is WSDualHttpBinding)
+            {
+                return;
+            }
+
+            if (binding is NetTcpBinding)
+            {
+                NetTcpBinding netTcpBinding = binding as NetTcpBinding;
+                if (netTcpBinding.ReliableSession.Enabled)
+                {
+                    return;
+                }
+            }
+
+            if (binding is WSHttpBindingBase)
+            {
+                WSHttpBindingBase wsHttpBindingBase = binding as WSHttpBindingBase;
+                if (wsHttpBindingBase.ReliableSession.Enabled)
+                {
+                    return;
+                }
+            }
+
+            throw new InvalidOperationException(Strings.ReliabilityNotSuportedOrDisabled);
+        }
+
+        public static void ValidateTransactionFlowEnabled(ServiceEndpoint serviceEndpoint)
+        { 
+        }
+
+        public static void ValidateOnlyWcfCommunication(ServiceEndpoint serviceEndpoint)
+        {
+            Binding binding = serviceEndpoint.Binding;
+            if (binding is NetMsmqBinding || binding is NetNamedPipeBinding || binding is NetTcpBinding)
+            {
+                return;
+            }
+
+            throw new InvalidOperationException(Strings.OnlyWcfCommunicationRequired);
+        }
+    }
+}
