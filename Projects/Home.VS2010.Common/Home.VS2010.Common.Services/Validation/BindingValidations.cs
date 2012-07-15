d12e746047109efbe57a075c644a86797420decd
André Marques de Araújo <andremarques023@gmail.com>
2012-07-15 03:17:08 -0300

    Common.Services - BindingRequirementAttribute.cs
    ValidateTransactionFlowEnabled() method implemented.

--- Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Validation/BindingValidations.cs
+++ Projects/Home.VS2010.Common/Home.VS2010.Common.Services/Validation/BindingValidations.cs
@@ -11,6 +11,7 @@ namespace Home.VS2010.Common.Services.Validation
     using System.ServiceModel;
     using System.ServiceModel.Channels;
     using System.ServiceModel.Description;
+    using Microsoft.CSharp.RuntimeBinder;
     using Resources;
 
     /// <summary>
@@ -18,32 +19,34 @@ namespace Home.VS2010.Common.Services.Validation
     /// </summary>
     public static class BindingValidations
     {
+        /// <summary>
+        /// TODO: Update summary.
+        /// </summary>
+        /// <param name="serviceEndpoint">The service endpoint.</param>
         public static void ValidateReliabilityEnabled(ServiceEndpoint serviceEndpoint)
         {
-            Binding binding = serviceEndpoint.Binding;
-
-            if (binding is NetNamedPipeBinding)
+            if (serviceEndpoint.Binding is NetNamedPipeBinding)
             {
                 return;
             }
 
-            if (binding is WSDualHttpBinding)
+            if (serviceEndpoint.Binding is WSDualHttpBinding)
             {
                 return;
             }
 
-            if (binding is NetTcpBinding)
+            if (serviceEndpoint.Binding is NetTcpBinding)
             {
-                NetTcpBinding netTcpBinding = binding as NetTcpBinding;
+                NetTcpBinding netTcpBinding = serviceEndpoint.Binding as NetTcpBinding;
                 if (netTcpBinding.ReliableSession.Enabled)
                 {
                     return;
                 }
             }
 
-            if (binding is WSHttpBindingBase)
+            if (serviceEndpoint.Binding is WSHttpBindingBase)
             {
-                WSHttpBindingBase wsHttpBindingBase = binding as WSHttpBindingBase;
+                WSHttpBindingBase wsHttpBindingBase = serviceEndpoint.Binding as WSHttpBindingBase;
                 if (wsHttpBindingBase.ReliableSession.Enabled)
                 {
                     return;
@@ -53,10 +56,42 @@ namespace Home.VS2010.Common.Services.Validation
             throw new InvalidOperationException(Strings.ReliabilityNotSuportedOrDisabled);
         }
 
+        /// <summary>
+        /// TODO: Update summary.
+        /// </summary>
+        /// <param name="serviceEndpoint">The service endpoint.</param>
         public static void ValidateTransactionFlowEnabled(ServiceEndpoint serviceEndpoint)
-        { 
+        {
+            foreach (var operation in serviceEndpoint.Contract.Operations)
+            {
+                TransactionFlowAttribute transactionFlowAttribute = operation.Behaviors.Find<TransactionFlowAttribute>();
+                if (transactionFlowAttribute != null)
+                {
+                    if (transactionFlowAttribute.Transactions == TransactionFlowOption.Allowed)
+                    {
+                        try
+                        {
+                            dynamic binding = serviceEndpoint.Binding;
+                            if (!binding.TransactionFlow)
+                            {
+                                throw new InvalidOperationException(Strings.TransactionFlowNotSupportedOrDisable);
+                            }
+
+                            continue;
+                        }
+                        catch (RuntimeBinderException)
+                        {
+                            throw new InvalidOperationException(Strings.TransactionFlowNotSupportedOrDisable);
+                        }
+                    }
+                }
+            }
         }
 
+        /// <summary>
+        /// TODO: Update summary.
+        /// </summary>
+        /// <param name="serviceEndpoint">The service endpoint.</param>
         public static void ValidateOnlyWcfCommunication(ServiceEndpoint serviceEndpoint)
         {
             Binding binding = serviceEndpoint.Binding;
